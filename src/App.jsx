4dbbd916bb3456f87f64ce743ac7d4dbf6f492a0
Gary Wintle <gwintle@gmail.com>
2025-07-19 17:30:40 +0900

    refactoring react

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,42 @@ import { useState } from 'react';
 import PomoText from './components/PomoText/PomoText';
 import Pomotomo from './components/Pomotomo/Pomotomo';
 import TaskInput from './components/TaskInput/TaskInput';
+import TextInput from './components/TextInput/TextInput';
+import TimeInput from './components/TimeInput/TimeInput';
 import TaskList from './components/TaskList/TaskList';
 
 function App() {
   const [task, setTask] = useState([]);
-  const [activeMachine, setActiveMachine] = useState('bounceAnim');
+  const [activeMachine, setActiveMachine] = useState('idleAnim');
+  const [taskText, setTaskText] = useState('');
+  const [taskTime, setTaskTime] = useState(0);
 
   function handleAddTask(newTask) {
     setTask((tasks) => [...tasks, newTask]);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!taskText.trim()) return;
+    const newTask = { taskText, taskTime, completed: false, id: Date.now() };
+    console.log(newTask);
+    handleAddTask(newTask);
+    setTaskText('');
+    setTaskTime(0);
   }
 
   return (
     <div className="main-container">
       <PomoText />
       <Pomotomo activeMachine={activeMachine} />
-      <TaskInput
-        onAddTask={handleAddTask}
-        setActiveMachine={setActiveMachine}
-      />
+      <TaskInput onSubmit={handleSubmit}>
+        <TextInput
+          setActiveMachine={setActiveMachine}
+          taskText={taskText}
+          setTaskText={setTaskText}
+        />
+        <TimeInput taskTime={taskTime} setTaskTime={setTaskTime} />
+      </TaskInput>
       <TaskList task={task} onAddTask={handleAddTask} />
     </div>
   );
--- src/components/TaskInput/TaskInput.jsx
+++ src/components/TaskInput/TaskInput.jsx
@@ -1,38 +1,15 @@
-import { useState } from 'react';
 import styles from './TaskInput.module.css';
 
-import TextInput from '../TextInput/TextInput';
-import TimeInput from '../TimeInput/TimeInput';
-
-function TaskInput({ onAddTask, setActiveMachine }) {
-  const [taskText, setTaskText] = useState('');
-  const [taskTime, setTaskTime] = useState(0);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (!taskText.trim()) return;
-    const newTask = { taskText, taskTime, completed: false, id: Date.now() };
-    console.log(newTask);
-    onAddTask(newTask);
-    setTaskText('');
-    setTaskTime(0);
-  }
-
+const TaskInput = ({ onSubmit, children }) => {
   return (
-    <form className={styles.container} onSubmit={handleSubmit}>
-      <TextInput
-        taskText={taskText}
-        setTaskText={setTaskText}
-        setActiveMachine={setActiveMachine}
-      />
-
-      <TimeInput taskTime={taskTime} setTaskTime={setTaskTime} />
+    <form className={styles.container} onSubmit={onSubmit}>
+      {children}
 
       <button type="submit" className={styles.button}>
         Add Task
       </button>
     </form>
   );
-}
+};
 
 export default TaskInput;
